fbc99f9a639a44f451a5cb251292a3882e86da37
glrarn <131609054+glrarn@users.noreply.github.com>
2023-04-24 01:00:51 +0300

    Update test01.java

--- src/main/java/TestClass/test01.java
+++ src/main/java/TestClass/test01.java
@@ -3,6 +3,7 @@ package TestClass;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("Git den merhaba");
+         System.out.println("GitHub dan merhaba");
         System.out.println();
     }
 }
